1ef2275803bc0f96caf314a81aec54fd1e4aa237
Pradeep912830 <pradeepsaaho432@gmail.com>
2025-06-24 19:32:59 +0530

    All Item part created

--- app/src/main/java/com/example/foodorderingadmin/AdminAddItem.java
+++ app/src/main/java/com/example/foodorderingadmin/AdminAddItem.java
@@ -1,11 +1,7 @@
 package com.example.foodorderingadmin;
 
 import android.annotation.SuppressLint;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,32 +9,20 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-
-    LinearLayout addItem;
+public class AdminAddItem extends AppCompatActivity {
 
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_admin_add_iteam);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        addItem = findViewById(R.id.addItem);
-
-
-        addItem.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, AdminAddItem.class);
-                startActivity(intent);
-            }
-        });
 
 
     }
