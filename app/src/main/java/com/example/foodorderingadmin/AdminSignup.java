462409a26bfeab574fd7b8f97534367ca9c64c37
Pradeep912830 <pradeepsaaho432@gmail.com>
2025-07-13 16:15:26 +0530

    Provide space in signup

--- app/src/main/java/com/example/foodorderingadmin/AdminSignup.java
+++ app/src/main/java/com/example/foodorderingadmin/AdminSignup.java
@@ -27,7 +27,7 @@ public class AdminSignup extends AppCompatActivity {
     private TextView alreadyHaveAccount;
 
     String[] locations = {"Mumbai", "Delhi", "Bangalore", "Hyderabad", "Ahmedabad", "Chennai", "Kolkata", "Pune", "Jaipur", "Surat",
-            "Lucknow", "Kanpur", "Nagpur", "Indore", "Thane", "Bhopal", "Visakhapatnam", "Patna", "Vadodara", "Ghaziabad",
+            "Lucknow", "Kanpur git ", "Nagpur", "Indore", "Thane", "Bhopal", "Visakhapatnam", "Patna", "Vadodara", "Ghaziabad",
             "Ludhiana", "Agra", "Nashik", "Faridabad", "Meerut", "Rajkot", "Kalyan-Dombivli", "Vasai-Virar", "Varanasi", "Srinagar",
             "Aurangabad", "Dhanbad", "Amritsar", "Navi Mumbai", "Allahabad", "Ranchi", "Howrah", "Coimbatore", "Jabalpur", "Gwalior",
             "Vijayawada", "Jodhpur", "Madurai", "Raipur", "Kota", "Guwahati", "Chandigarh", "Solapur", "Hubli–Dharwad", "Tiruchirappalli"}; // example cities
